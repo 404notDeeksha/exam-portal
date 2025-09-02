20fddf0fb15d630cae0d8ee77c5c83607a4145ce
Deeksha <sharmadeekksha@gmail.com>
2025-09-02 15:15:18 +0530

    chore: mongodb connection established

---
+++ backend/config/dbConnection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+  console.log("🟡 Connecting to MongoDB...", process.env.MONGODB_URL);
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB connection successful!");
+  } catch (err) {
+    console.log("MongoDB connection failed:", err);
+  }
+};
+
+export default dbConnection;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
-import app from "./server.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+import app from "./server.js";
+import dbConnection from "./config/dbConnection.js";
+
+dbConnection();
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
