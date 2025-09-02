b16e21d3c967abd0be4710376b31189bb4c8cced
Deeksha <sharmadeekksha@gmail.com>
2025-09-02 19:02:02 +0530

    fix: updated method pf api  questions

--- backend/routes/Question.routes.js
+++ backend/routes/Question.routes.js
@@ -3,7 +3,7 @@ const examRouter = express.Router();
 
 import { getQuestions, submitExam } from "../controllers/Question.controller.js";
 
-examRouter.route("/questions").post(getQuestions);
+examRouter.route("/questions").get(getQuestions);
 examRouter.route("/submit").post(submitExam);
 
 export default examRouter;
