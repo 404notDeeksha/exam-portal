f6b2c111ea3607ae0442b4e80a4b8399f6d3723f
Deeksha <sharmadeekksha@gmail.com>
2025-09-03 19:00:47 +0530

    chore: centralised err handling & token retrival

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,35 @@
+// Centralized API handler with interceptors for errors and JWT.
+import axios from "axios";
+
+// Common error handler
+const handleError = (error) => {
+  const message =
+    error.response?.data?.message || error.message || "Something went wrong";
+  return Promise.reject(message);
+};
+
+// Instance WITHOUT token (public)
+export const publicAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+// Instance WITH token (protected)
+export const privateAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+// Attach token to private requests
+privateAPI.interceptors.request.use((config) => {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+  
+  // Add error handling to both
+  publicAPI.interceptors.response.use((res) => res, handleError);
+  privateAPI.interceptors.response.use((res) => res, handleError);
+
