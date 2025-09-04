a7ac55e8acd2cc3a5733db85818697dab056d7b6
Deeksha <sharmadeekksha@gmail.com>
2025-09-04 16:57:35 +0530

    fix: user data wasnt persisting in storage

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,27 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
+const rootReducer = combineReducers({
+  auth: authReducer,
+  // add other reducers here
+});
+
 // --- Persist config ---
 const persistConfig = {
-  key: "root", // key in localStorage
-  storage, // storage engine (localStorage here)
-  whitelist: ["auth"], // state slices you want to persist
+  key: "root",
+  storage,
+  whitelist: ["auth"],
 };
 
 // Wrap reducer with persistReducer
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // --- Store ---
 export const store = configureStore({
-  reducer: {
-    auth: persistedAuthReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // required for redux-persist
+      serializableCheck: false,
     }),
 });
 
--- frontend/src/components/ProtectedRoutes/RequireAuth.jsx
+++ frontend/src/components/ProtectedRoutes/RequireAuth.jsx
@@ -4,6 +4,7 @@ import { routes } from "../../constants/routes";
 
 export const RequireAuth = () => {
   const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
 
   return token ? <Outlet /> : <Navigate to={routes.login} replace />;
 };
